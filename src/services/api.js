70f32114a878ee401ada7056b0501d99cdce1910
Pradeep Khatri <pradeepkhatri44@live.com>
2025-01-13 11:53:30 +0530

    full code for movie reviews
    movie reviews

---
+++ src/services/api.js
@@ -0,0 +1,16 @@
+const API_KEY = "d6bf00be29f6e304dcbc239d0368df80";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async () => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query={encodeURIComponent(query)}`
+  );
+  const data = await response.json();
+  return data.results;
+};
